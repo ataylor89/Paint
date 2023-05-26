8182df3c6ac7acfeeed390e05524598f45fb592e
Andrew Taylor <rolyata47@gmail.com>
2023-05-26 03:48:33 -0500

    Removing the 'abstract' modifier from method declarations

--- src/main/java/paint/algorithm/Algorithm.java
+++ src/main/java/paint/algorithm/Algorithm.java
@@ -1,6 +1,5 @@
 package paint.algorithm;
 
-import paint.Canvas;
 import java.awt.event.MouseEvent;
 
 /**
@@ -8,8 +7,8 @@ import java.awt.event.MouseEvent;
  * @author andrewtaylor
  */
 public interface Algorithm {
-    public abstract void mousePressed(MouseEvent event);
-    public abstract void mouseReleased(MouseEvent event);
-    public abstract void mouseMoved(MouseEvent event);
-    public abstract void mouseDragged(MouseEvent event);
+    public void mousePressed(MouseEvent event);
+    public void mouseReleased(MouseEvent event);
+    public void mouseMoved(MouseEvent event);
+    public void mouseDragged(MouseEvent event);
 }
