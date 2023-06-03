007053c33e68c8727fd96e5ebec12d11245b8a29
Andrew Taylor <rolyata47@gmail.com>
2023-06-02 19:07:10 -0500

    Simplifying the Selection class

--- src/main/java/paint/tools/Selection.java
+++ src/main/java/paint/tools/Selection.java
@@ -1,18 +1,13 @@
 package paint.tools;
 
-import java.io.Serializable;
-
 /**
  *
  * @author andrewtaylor
  */
-public class Selection implements Serializable {
+public class Selection {
     
     public int x, y, width, height;
-    private final static long serialVersionUID = 1L;
     
-    public Selection() {}
-       
     public Selection(int x, int y, int width, int height) {
         this.x = x;
         this.y = y;
