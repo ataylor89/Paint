8657641c399a3e1b6a48399b50dddd2b1f4ccebb
Andrew Taylor <rolyata47@gmail.com>
2025-08-12 21:15:02 -0500

    Updating build.sh

--- build.sh
+++ build.sh
@@ -1,11 +1,9 @@
 mvn clean install
-jpackage --input target/ \
+jpackage --input target \
   --name Paint \
   --main-jar Paint.jar \
   --main-class paint.App \
   --type dmg \
   --app-version "1.0.0" \
-  --vendor "Andrew's software" \
-  --copyright "Copyright 2023" \
   --mac-package-name "Paint" \
   --verbose
