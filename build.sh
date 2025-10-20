fe8516c0824291ad0d46a3f65111fa3b47d93fc9
Andrew Taylor <rolyata47@gmail.com>
2025-10-19 19:55:14 -0500

    Adding comments to build.sh

--- build.sh
+++ build.sh
@@ -6,8 +6,23 @@
 #
 # If I wanted to avoid this process, I could get an Apple Developer ID, and sign or notarize my application.
 # But that takes a lot of work... maybe I'll get to it in the future... but I don't know.
-
+#
+# ------
+#
+# The comments below explain the commands used in the build process
+#
+# ------
+#
+# The command "mvn clean install" reads our pom.xml file and looks for build instructions in the pom.xml file
+#
+# Specifically, it downloads every necessary dependency, compiles the source code into bytecode, and packages the bytecode into a jar file, which it saves in the target folder
+#
+# In addition, the jar file is configured to be executable, per the instructions contained within the pom.xml file
+#
 mvn clean install
+#
+# The command "jpackage" accepts a jar file as its argument and outputs a dmg file
+#
 jpackage --input target \
   --name Paint \
   --main-jar Paint.jar \
