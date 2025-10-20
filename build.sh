6c190eea956bd091e4431aaae6405aa77527517a
Andrew Taylor <rolyata47@gmail.com>
2025-10-19 19:18:19 -0500

    Adding comments to the build script

--- build.sh
+++ build.sh
@@ -1,3 +1,12 @@
+# This script creates a dmg installer file in the current directory.
+#
+# After installing the application to my /Applications folder,
+# I actually have to click on the Apple menu (in the top left corner of the menu bar),
+# and then click on System Settings, and then navigate to Privacy & Security, and allow the Paint application to be opened.
+#
+# If I wanted to avoid this process, I could get an Apple Developer ID, and sign or notarize my application.
+# But that takes a lot of work... maybe I'll get to it in the future... but I don't know.
+
 mvn clean install
 jpackage --input target \
   --name Paint \
