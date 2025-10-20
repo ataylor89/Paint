6234d8f84560e84ab3b0632524af474553468ea0
Andrew Taylor <rolyata47@gmail.com>
2025-10-20 04:49:14 -0500

    Simplifying code

--- src/main/java/paint/menuactions/FillSelection.java
+++ src/main/java/paint/menuactions/FillSelection.java
@@ -6,6 +6,7 @@ import java.awt.event.ActionEvent;
 import javax.swing.AbstractAction;
 import javax.swing.SwingUtilities;
 import paint.App;
+import paint.Settings;
 import paint.gui.Easel;
 import paint.gui.Canvas;
 import paint.tools.Marquee;
@@ -18,30 +19,29 @@ import paint.tools.Selection;
 public class FillSelection extends AbstractAction {
     
     private App app;
-    private Easel easel;
-    private Canvas canvas;
 
     public FillSelection(App app) {
         super("Fill selection");
         this.app = app;
-        easel = app.getEasel();
-        canvas = easel.getCanvas();
     }
     
     @Override
     public void actionPerformed(ActionEvent e) {
+        Settings settings = app.getSettings();
+        Easel easel = app.getEasel();
+        Canvas canvas = easel.getCanvas();
         canvas.repaint();
         SwingUtilities.invokeLater(() -> {
-            Marquee marquee = (Marquee) app.getEasel().getToolbox().get("Marquee");
+            Marquee marquee = (Marquee) easel.getToolbox().get("Marquee");
             Selection selection = marquee.calculate();
-            Color color = app.getSettings().getPaintColor();
+            Color color = settings.getPaintColor();
             Graphics canvasGraphics = canvas.getGraphics();
             Graphics imageGraphics = canvas.getLayeredImage().getForeground().getGraphics();
             canvasGraphics.setColor(color);
             canvasGraphics.fillRect(selection.x, selection.y, selection.width, selection.height);    
             imageGraphics.setColor(color);
             imageGraphics.fillRect(selection.x, selection.y, selection.width, selection.height);            
-            app.getSettings().setMarquee(false);
+            settings.setMarquee(false);
         });
     }
 }
